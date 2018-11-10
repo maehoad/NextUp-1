a186a4a37d225564bddf8160b798e193da0eae7c
diegofinni <44912645+diegofinni@users.noreply.github.com>
2018-11-10 13:55:38 -0500

    Add files via upload

---
+++ NextUp.py
@@ -0,0 +1,105 @@
+# README: The purpose of this file is to update the local json file to include all the songs in the Spotify playlist,
+# and then reorder the playlist to have the songs with the most votes appear first on the playlist.
+# Note this file does NOT add votes.
+# For this file to work in conjunction with the app you MUST call the file, then add a username ("diegofinni" in our
+# case), followed by an id of the playlist and an then an id of all the tracks you want to add to the playlist
+# Example of calling file: python3 NextUp.py diegofinni 5K3rtFT1Tq19lJ04wjuVBV 59WN2psjkt1tyaxjspN8fp
+# The client data needed for this file to work (CLIENT ID, SECRET, and REDIRECT_URI) are already given
+
+import sys
+import spotipy
+import spotipy.util as util
+from algor1 import *
+
+#######################################################################################################################
+# These functions fetch a json file and then add any song to the json
+# file that is not already on it and gives it a default value of 0
+
+filename = "votes.json"
+
+
+def get_list():
+    with open(filename) as file:
+        return json.load(file)
+
+
+def update_list(lst):
+    dic = get_list()
+    for song in lst:
+        if song not in dic:
+            dic[song] = 0
+
+    set_list(dic)
+
+
+def set_list(dic):
+    with open(filename, 'w') as file:
+        return json.dump(dic, file, indent=2)
+
+
+def read_playlist():
+    with open(filename, "r") as file:
+        return json.load(file)
+
+
+########################################################################################################################
+
+# This function sorts through the playlist to make
+# a list of the ids and a list of the song titles
+def show_tracks(tracks):
+    names = []
+    ids = []
+    for i, item in enumerate(tracks['items']):
+        track = item['track']
+        names += [track["id"]]
+        ids += [track["name"]]
+    return names, ids
+
+
+# This function reorders the Spotify playlist
+# based on votes found in the json file
+def reorder_playlist(names):
+    new_order = best_songs()
+    for i in range(len(new_order) -1, -1, -1):
+        for j in range(len(names)):
+            if new_order[i][0] == names[j]:
+                names.insert(0, names[j])
+                names.pop(j + 1)
+                sp.user_playlist_reorder_tracks(username, playlist, j, 0, 1)
+
+
+# This if/else makes sure all the Spotify data (id, secret, uri) are inputted
+# Note that it is assumed you are inputting a username, playlist, and a list of track_ids
+if len(sys.argv) > 3:
+    username = sys.argv[1]
+    playlist = sys.argv[2]
+    track_ids = sys.argv[3:]
+else:
+    print("Usage: %s username playlist_id track_id ..." % (sys.argv[0],))
+    sys.exit()
+
+scope = 'playlist-modify-public'
+token = util.prompt_for_user_token(username, scope, client_id='171b3cdfebb344ba9772c6859136c2d4',
+                                   client_secret='Fca0bb0ea99f493d987b27c9568b79ee',
+                                   redirect_uri='http://www.google.com/')
+
+# If a valid token is used, this if statement compiles all of the helper
+#  functions to execute the purpose of this file(read README if confused)
+if token:
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    results = sp.user_playlist(username, playlist, fields="tracks,next")
+    tracks = results['tracks']
+    song_ids = show_tracks(tracks)[0]
+    names = show_tracks(tracks)[1]
+    for track in track_ids:
+        if track in song_ids:
+            track_ids.remove(track)
+    if len(track_ids) >= 1:
+        results = sp.user_playlist_add_tracks(username, playlist, track_ids)
+    update_list(names)
+    reorder_playlist(names)
+else:
+    print("Can't get token for", username)
+
+########################################################################################################################
